a7dd1d24c3f8f7c9168467f448bf6f2635bbc4f1
Telishchak <yurii.telishchak@aptiv.com>
2021-09-22 10:01:07 +0300

    CustomerRepository.java created, Customer.java edited

--- src/main/java/com/example/crudwithvaadin/Customer.java
+++ src/main/java/com/example/crudwithvaadin/Customer.java
@@ -33,6 +33,7 @@ public class Customer {
 
     @Override
     public String toString(){
-        return String.format("Customer[id=%d, firstName='%s', lastName='%s'");
+
+        return String.format("Customer[id=%d, firstName='%s', lastName='%s']");
     }
 }
